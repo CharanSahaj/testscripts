44619368831537b4dab38c881509160c3f6fdca1
Charan Nama Arunkumar <charana@Admins-MacBook-Pro.local>
2017-08-10 12:01:59 +0530

    fix for the wait in the webserver thing

--- launch_vm.sh
+++ launch_vm.sh
@@ -1,4 +1,4 @@
 az group create --name myResourceGroup --location eastus
-az vm create --resource-group myResourceGroup --name myVM --image UbuntuLTS | python3 -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)['publicIpAddress']))">ips
-az vm open-port --port 80 --resource-group myResourceGroup --name myVM
-az vm extension set --resource-group myResourceGroup --vm-name myVM --name customScript --publisher Microsoft.Azure.Extensions --settings ./test.json
+az vm create --resource-group myResourceGroup --name myVM3 --image UbuntuLTS | python3 -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)['publicIpAddress']))">ips
+az vm open-port --port 80 --resource-group myResourceGroup --name myVM3
+az vm extension set --resource-group myResourceGroup --vm-name myVM3 --name customScript --publisher Microsoft.Azure.Extensions --no-wait --settings ./test.json
