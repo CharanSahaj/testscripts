25c3fcd780bc48412200d313a49fa41d213e2930
Charan Nama Arunkumar <charana@Admins-MacBook-Pro.local>
2017-08-10 12:14:46 +0530

    added a comment for later

--- launch_vm.sh
+++ launch_vm.sh
@@ -2,3 +2,4 @@ az group create --name myResourceGroup --location eastus
 az vm create --resource-group myResourceGroup --name myVM3 --image UbuntuLTS | python3 -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)['publicIpAddress']))">ips
 az vm open-port --port 80 --resource-group myResourceGroup --name myVM3
 az vm extension set --resource-group myResourceGroup --vm-name myVM3 --name customScript --publisher Microsoft.Azure.Extensions --no-wait --settings ./test.json
+#az vm list-ip-addresses to list-ip-addresses
